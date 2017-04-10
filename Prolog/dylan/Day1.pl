324dbc42ef8b3bacb3e2b5dd3b109d26fb1dfd91
DylanBohlender <dbohlender@hotmail.com>
2017-04-10 01:28:00 -0600

    Day 1 Prolog commit

---
+++ Prolog/dylan/Day1.pl
@@ -0,0 +1,31 @@
+% Compile with `gplc Day1.pl`; run with `./Day1` (might need to chmod the executable)
+
+% Part 1
+
+book(good_to_great, jim_collins).
+book(influence, robert_cialdini).
+book(straight_to_hell, john_lefevre).
+book(playing_to_win, david_sirlin).
+book(harry_potter_one, j_k_rowling).
+book(harry_potter_two, j_k_rowling).
+
+% Run this in the interpreter:
+%   book(Book, j_k_rowling).
+
+
+% Part 2
+
+instrument(run_the_jewels, voice).
+instrument(jauz, computer).
+instrument(blau, computer).
+instrument(zac_brown_band, guitar).
+instrument(bag_raiders, computer).
+
+genre(run_the_jewels, hip_hop).
+genre(jauz, edm).
+genre(blau, edm).
+genre(zac_brown_band, country).
+genre(bag_raiders, edm).
+
+% Run this in the interpreter:
+%   instrument(Musician, guitar).
